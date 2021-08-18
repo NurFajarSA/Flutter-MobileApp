3a2ebb14892284a1bb25e83ccfb987faf8d35f00
Nur Fajar S. A <nur.fajar11@ui.ac.id>
2021-08-18 22:22:40 +0700

    Menambahkan Icon Button

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -17,6 +18,12 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("First Mobile Flutter"),
           backgroundColor: Colors.grey,
+          actions: <Widget>[
+            IconButton(onPressed: () {}, icon: Icon(Icons.mail))
+          ],
+        ),
+        body: SafeArea(
+          child: Text("Flutter"),
         ),
       ),
     );
