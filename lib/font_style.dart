675852ab8e0de42b4cd7adcd882ede1822fbdd1f
Nur Fajar S A <nur.fajar11@ui.ac.id>
2021-08-22 15:46:55 -0700

    Change color

--- lib/font_style.dart
+++ lib/font_style.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 TextStyle mainHeader = TextStyle(
     fontSize: 26,
-    color: Colors.lightBlue,
+    color: Colors.green,
     fontFamily: 'Poppins',
     fontWeight: FontWeight.w700);
 
 TextStyle subHeader = TextStyle(
     fontSize: 16,
-    color: Colors.lightBlueAccent,
+    color: Colors.green.shade500,
     fontFamily: 'Poppins',
     fontWeight: FontWeight.w300);
